c507f6a0fce65ff30bcb6b442eeadb7cd497cfcd
Aditya Agarwal <aditya.7@outlook.com>
2021-07-16 18:56:59 +0530

    Added mongodb models and passport local

--- elections/server.js
+++ elections/server.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
+const expressSanitizer = require('express-sanitizer')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const session = require('express-session')
+const passport = require('passport')
+const mongoose = require('mongoose')
+const KEYS = require("./config/keys");
 
-var expressSanitizer = require('express-sanitizer')
-var bodyParser = require('body-parser')
-var methodOverride = require('method-override')
 
-
-app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'))
 app.use(expressSanitizer());
 app.use(bodyParser.urlencoded({
@@ -15,12 +17,27 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(bodyParser.json())
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'hold the door',
+  resave: false,
+  saveUninitialized: true,
+}))
 
+// Passport Middleware
+require("./config/passport")(passport)
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect(KEYS.MongoURI || process.env.MongoURI , { useUnifiedTopology: true , useNewUrlParser: true })
+.then(()=> {console.log("MongoDB Connected")})
+.catch(err=>{
+  console.log(err)
+})
 
 // CORS enabled
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,CSRF-Token, credentials, useCredentials");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
@@ -31,7 +48,6 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
 })
