c568d03d195e6e56b5bf6dac8577924c334d6f0b
Aditya Agarwal <aditya.7@outlook.com>
2021-07-17 20:13:50 +0530

    Updated header component to reflect logged in and out bnuttons

--- elections/src/app/header/header.component.ts
+++ elections/src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService} from "../login/login.service";
+import { AuthenticateService } from "../common/authenticate.service";
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  loggedIn : boolean = false;
+
+  constructor(private loginservice: LoginService,
+  private authenticateservice : AuthenticateService) {
+    this.authenticateservice.User.subscribe(userData=>{
+      if(userData){
+        this.loggedIn = true;
+      }else{
+        this.loggedIn = false;
+      }
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  logout(){
+    this.loginservice.SignOut()
   }
 
 }
