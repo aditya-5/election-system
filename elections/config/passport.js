7396feadce645c39351717c7e6c6d1e05dff5eeb
Aditya Agarwal <aditya.7@outlook.com>
2021-07-18 18:18:33 +0530

    Modified local strategy to allow for multiple strategies athe same time

--- elections/config/passport.js
+++ elections/config/passport.js
@@ -1,17 +1,18 @@
 const LocalStrategy = require('passport-local').Strategy
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
-const User = require('../models/VoterUser')
+const VoterUser = require('../models/VoterUser')
+const SocietyUser = require('../models/SocietyUser')
 
 
-module.exports = function(passport){
-  passport.use(
+let voterLocal=
+function(passport){
+  passport.use("voter-local",
     new LocalStrategy({usernameField : "email"}, (email, password, done)=>{
       // Match the email
-      User.findOne({email: email})
+      VoterUser.findOne({email: email})
       .then(user =>{
         if(!user){
-          console.log("jhello")
           return done(null, false, {message : "The email is not registered"})
         }
 
@@ -39,10 +40,69 @@ module.exports = function(passport){
   });
 
   passport.deserializeUser((id, done)=>{
-    User.findById(id, (err, user)=> {
+    VoterUser.findById(id, (err, user)=> {
       done(err, user);
     });
   });
 
 
+}
+
+
+
+
+let societyLocal= function(passport){
+  passport.use("society-local",
+    new LocalStrategy({usernameField : "email"}, (email, password, done)=>{
+      // Match the email
+      SocietyUser.findOne({email: email})
+      .then(user =>{
+        if(!user){
+          return done(null, false, {message : "The email is not registered"})
+        }
+
+        //Match password
+        bcrypt.compare(password, user.password, (err, isMatch)=>{
+
+          if(err) throw err;
+          if(isMatch){
+            if(user.isVerified){
+              return done(null, user)
+            }else{
+              return done(null, false, { message: "The account has not been verified. Please check your email or request a new link."})
+            }
+          }else{
+            return done(null, false, { message: "Invalid combination of password/email. Please try again."})
+          }
+        })
+      })
+      .catch(err=> console.log(err))
+    })
+  )
+
+  passport.serializeUser((user, done)=>{
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done)=>{
+    SocietyUser.findById(id, (err, user)=> {
+
+      if(err) done(err)
+      if(user) done (null, user)
+      else{
+        VoterUser.findById(id, (err, user)=>{
+          if(err) done(err)
+          done(null, user)
+        } )
+      }
+      // done(err, user);
+    });
+  });
+
+
+}
+
+module.exports = {
+  voterLocal,
+  societyLocal
 }
