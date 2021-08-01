05e4903c57c0e51282e8f919a0f395322b07c487
Aditya Agarwal <aditya.7@outlook.com>
2021-08-01 06:19:45 +0530

    Funcationality for voter and sciety updation, added frontend checking

--- elections/src/app/portal/settings/settings.component.ts
+++ elections/src/app/portal/settings/settings.component.ts
@@ -3,7 +3,7 @@ import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthenticateService } from "../../common/authenticate.service"
 import { PortalService } from "../../common/portal.service"
-import {FormGroup, FormControl} from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -14,25 +14,26 @@ import { Observable } from 'rxjs';
 export class SettingsComponent implements OnInit {
 
 
-  userData : any;
-  submitted:boolean = false;
-  untilNextSet : any
+  userData: any;
+  submitted: boolean = false;
+  untilNextSet: any
   newpass: string;
-  confirmpass:string;
-  positionOther: boolean = false
+  confirmpass: string;
+  positionOther: boolean = false;
+  unSaved: boolean = false;
   @ViewChild('Messages', { static: false }) Messages: ElementRef
   @ViewChild('settingsF', { static: false }) settingsForm: NgForm;
 
-  constructor(private authenticateservice : AuthenticateService,
-              private renderer: Renderer2,
-              private router: Router,
-              private portalservice : PortalService) {
+  constructor(private authenticateservice: AuthenticateService,
+    private renderer: Renderer2,
+    private router: Router,
+    private portalservice: PortalService) {
 
 
   }
 
-  checkPassword(){
-    if((this.newpass != this.confirmpass) || this.newpass.length<6 || this.confirmpass.length<6) return true
+  checkPassword() {
+    if ((this.newpass != this.confirmpass) || this.newpass.length < 6 || this.confirmpass.length < 6) return true
     else return false
   }
 
@@ -50,86 +51,189 @@ export class SettingsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.authenticateservice.User.subscribe(userData=>{
-      if(userData) {
-                    this.userData = userData
-                    this.untilNextSet = userData.untilNextSet
-                    if(userData.position=="Other") this.positionOther = true
-                    setTimeout(()=>{
-                      this.settingsForm.form.patchValue({fname: userData.name, societyName: userData.societyName, position: userData.position, positionText:userData.positionText })
+    this.authenticateservice.User.subscribe(userData => {
+      if (userData) {
+        this.userData = userData
+        this.untilNextSet = userData.untilNextSet
+        if (userData.position == "Other") this.positionOther = true
+        setTimeout(() => {
+          this.settingsForm.form.patchValue({ fname: userData.name, societyName: userData.societyName, position: userData.position, positionText: userData.positionText })
 
-                    }, 10)
+        }, 10)
       }
     })
   }
 
+  validateFields(newData) {
+    if (newData.type == "society") {
+      const positionArray = ['President', "General Secretary", "Other"]
 
-  saveSettings(settingsF: NgForm){
+      if (newData.name.length < 1 || newData.societyName.length < 1 || newData.position.length < 1) {
+        this.submitted = false
+        setTimeout(() => {
+          this.appendErrorMessages("The fields cannot be empty.")
+        }, 2000)
+        return false;
+      }
+
+
+      if(newData.name == this.userData.name && newData.societyName == this.userData.society.societyName &&
+      newData.position == this.userData.position){
+        if(newData.position == "Other"){
+          if(newData.positionText == this.userData.positionText){
+            this.submitted = false
+            setTimeout(() => {
+              this.appendErrorMessages("Please update atleast 1 field.")
+            }, 2000)
+            return false;
+          }
+        }else{
+          this.submitted = false
+          setTimeout(() => {
+            this.appendErrorMessages("Please update atleast 1 field.")
+          }, 2000)
+          return false;
+        }
+      }
+
+      if (newData.societyName.length > 30) {
+        this.submitted = false
+        setTimeout(() => {
+          this.appendErrorMessages("Society Name should be less than 30 characters")
+        }, 2000)
+        return false;
+      }
+
+      if (newData.name.length > 20) {
+        this.submitted = false
+        setTimeout(() => {
+          this.appendErrorMessages("Name should be less than 20 characters")
+        }, 2000)
+        return false;
+      }
+
+      if (!positionArray.includes(newData.position)) {
+        this.submitted = false
+        setTimeout(() => {
+          this.appendErrorMessages("The value of radio buttons cannot be modified.")
+        }, 2000)
+        return false;
+      }
+
+      if (newData.position == "Other") {
+        if (newData.positionText.length < 1) {
+          this.submitted = false
+          setTimeout(() => {
+            this.appendErrorMessages("Other Role cannot be empty.")
+          }, 2000)
+          return false;
+        }
+      }
+
+      return true;
+
+    } else if (newData.type == "voter") {
+
+      if(newData.name == this.userData.name){
+        this.submitted = false
+        setTimeout(() => {
+          this.appendErrorMessages("Please update atleast 1 field.")
+        }, 2000)
+        return false;
+      }
+
+      if (newData.name.length < 1) {
+        this.submitted = false
+        setTimeout(() => {
+          this.appendErrorMessages("Name cannot be empty.")
+        }, 2000)
+        return false;
+      }
+
+      if (newData.name.length > 20) {
+        this.submitted = false
+        setTimeout(() => {
+          this.appendErrorMessages("Name should be less than 20 characters")
+        }, 2000)
+        return false;
+      }
+      return true;
+    } else {
+      this.submitted = false
+      setTimeout(() => {
+        this.appendErrorMessages("Invalid request.")
+      }, 2000)
+      return false;
+    }
+  }
+
+
+  saveSettings(settingsF: NgForm) {
     this.submitted = true
 
     const name = settingsF.value.fname
     const societyName = settingsF.value.societyName
     const position = settingsF.value.position
     const positionText = settingsF.value.positionText
-    const positionArray = ['President', "General Secretary","Other"]
 
-    if(name.length<1 || societyName.length< 1 || position.length< 1 ){
-      this.submitted = false
-      setTimeout(()=>{
-        this.appendErrorMessages("The fields cannot be empty.")
-      }, 2000)
-      return
-    }
 
-    if(!positionArray.includes(position)){
-      this.submitted = false
-      setTimeout(()=>{
-        this.appendErrorMessages("The value of radio buttons cannot be modified.")
-      }, 2000)
-      return
-    }
-
-    if(position == "Other"){
-      if(positionText.length<1){
-        this.submitted = false
-        setTimeout(()=>{
-          this.appendErrorMessages("Other Role cannot be empty.")
-        }, 2000)
-        return
+    let updateData;
+    if (this.userData.type == "society") {
+      updateData = {
+        name,
+        societyName,
+        position,
+        positionText,
+        type: "society"
+      }
+    } else {
+      updateData = {
+        name,
+        type: "voter"
       }
     }
 
-    const updateData = {
-      name,
-      societyName,
-      position,
-      positionText,
-      type:"society"
-    }
+    if (this.validateFields(updateData)) {
 
-    this.portalservice.updateSettings(updateData).subscribe((response)=>{
-      this.submitted = false
-      setTimeout(()=>{
-        this.appendSuccessMessages(response["message"])
-        this.authenticateservice.setUserWithoutSubs()
-      }, 2000)
-      settingsF.reset()
-    }, err=>{
-      this.submitted = false
-      setTimeout(()=>{
-        this.appendErrorMessages(err.error.message)
-        this.authenticateservice.setUserWithoutSubs()
-      }, 2000)
-      return
-    })
+      this.portalservice.updateSettings(updateData).subscribe((response) => {
+        this.submitted = false
+        setTimeout(() => {
+          this.appendSuccessMessages(response["message"])
+          this.authenticateservice.setUserWithoutSubs()
+        }, 2000)
+        settingsF.reset()
+      }, err => {
+        this.submitted = false
+        setTimeout(() => {
+          this.appendErrorMessages(err.error.message)
+          this.authenticateservice.setUserWithoutSubs()
+        }, 2000)
+        return
+      })
+
+
+    }
   }
+
+  canDeactivate(): Observable<boolean> | boolean {
+       if (this.unSaved) {
+         return confirm('There are unsaved changes! Are you sure you want to discard them?');
+       }
+       return true;
+   }
 
   togglePosition(positionOtherArg: boolean) {
     this.positionOther = positionOtherArg
   }
 
+  unnsavedChanges(positionOtherArg: boolean) {
+    this.unSaved = true;
+  }
 
-  changePassword(passwordF: NgForm){
+
+
+
+  changePassword(passwordF: NgForm) {
 
   }
 
